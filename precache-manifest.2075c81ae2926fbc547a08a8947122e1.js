e998ebd59ae40d58b134cedbc77cdaa4faafcd29
keupa <kapirupia@gmail.com>
2020-07-24 23:45:56 -0500

    Updates

---
+++ precache-manifest.2075c81ae2926fbc547a08a8947122e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b22b158438b13a1b3dafb99c5637ff",
+    "url": "/merry-go-round/index.html"
+  },
+  {
+    "revision": "d8affef5e7829fce1bdc",
+    "url": "/merry-go-round/static/css/main.62a46526.chunk.css"
+  },
+  {
+    "revision": "4ef02f7fb335b5772e59",
+    "url": "/merry-go-round/static/js/2.9b8569e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/merry-go-round/static/js/2.9b8569e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8affef5e7829fce1bdc",
+    "url": "/merry-go-round/static/js/main.c30069cf.chunk.js"
+  },
+  {
+    "revision": "f93b3566f554460e4dcd",
+    "url": "/merry-go-round/static/js/runtime-main.53bf17a9.js"
+  }
+]);
